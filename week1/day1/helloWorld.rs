b09615e9b098f81d8fa6b00af212ebafe1dbf04a
Kish Sal <kishan.salgado@gmail.com>
2023-08-30 11:05:51 -0400

    Update helloWorld.rs

--- week1/day1/helloWorld.rs
+++ week1/day1/helloWorld.rs
@@ -3,3 +3,6 @@ fn main() {
 }
 
 // To run a file enter: rustc helloWorld.rs
+// It then creates an executable file called helloWorld
+// To run the executable file enter: ./helloWorld
+// It then prints out Hello, world!
